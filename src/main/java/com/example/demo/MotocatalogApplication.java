72de37fc313cf11cf467d247a76d5617d942f45c
kaikyou76 <kaikyou@gmail.com>
2023-05-30 09:00:55 +0900

    初回commit

---
+++ src/main/java/com/example/demo/MotocatalogApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotocatalogApplication.class, args);
+	}
+
+}
